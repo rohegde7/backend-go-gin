036e202b46d2058730effd1caced3bfd8dbcc33f
rvd <hegde.rohit7@gmail.com>
2024-06-20 07:03:36 +0530

    Now this repo is updated with get with an ID and adding of a new album

--- main.go
+++ main.go
@@ -23,10 +23,34 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/albums", getAlbums)
+	router.POST("/addalbum", addAlbum)
+	router.GET("/albums/:id", getAlbumById)
 
 	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func addAlbum(ctx *gin.Context) {
+	var newAlbum album
+	if err := ctx.BindJSON(&newAlbum); err != nil {
+		//ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		albums = append(albums, newAlbum)
+	}
+	//ctx.JSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+	for _, album := range albums {
+		if album.ID == id {
+			c.JSON(http.StatusOK, album)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNoContent, gin.H{"message": "Album not found"})
 }
